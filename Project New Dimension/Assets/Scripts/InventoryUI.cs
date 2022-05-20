5506c1b63e06a67f803fb83a5af8fe62b6c2ebc5
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2022-05-20 20:04:47 +0200

    Dodano otwieranie/zamykanie ekwipunku

--- Project New Dimension/Assets/Scripts/InventoryUI.cs
+++ Project New Dimension/Assets/Scripts/InventoryUI.cs
@@ -6,27 +6,28 @@ using UnityEngine.UI;
 public class InventoryUI : MonoBehaviour
 {
     [SerializeField]
-    ItemSlot[] itemsSlot;
+    ItemSlot[] itemsSlots;
 
     Inventory inventory;
     void Start()
     {
         inventory = Inventory.instance;
         inventory.OnInventoryChanged += Refresh;
-        itemsSlot = GetComponentsInChildren<ItemSlot>();
+        itemsSlots = GetComponentsInChildren<ItemSlot>();
+        gameObject.SetActive(false);
     }
 
     void Refresh()
     {
-        for (int i = 0; i < itemsSlot.Length; i++)
+        for (int i = 0; i < itemsSlots.Length; i++)
         {
             if (i < inventory.items.Count)
             {
-                itemsSlot[i].AddItem(inventory.items[i]);
+                itemsSlots[i].AddItem(inventory.items[i]);
             }
             else
             {
-                itemsSlot[i].RemoveItem();
+                itemsSlots[i].RemoveItem();
             }
         }
     }
