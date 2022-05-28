c2d12715ac2eb4076572c8da5c9905f1a26e755e
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2022-05-28 17:58:19 +0200

    Dodano podstawowy atak

---
+++ Project New Dimension/Assets/Scripts/Attack.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Attack : MonoBehaviour
+{
+    [SerializeField]
+    bool isPlayer = false;
+
+    [SerializeField]
+    int basicAttackCooldown = 2;
+
+    int actualBasicAttackCooldown;
+    BoxCollider attackArea;
+
+    void Start()
+    {
+        attackArea = gameObject.GetComponent<BoxCollider>();
+        attackArea.isTrigger = true;
+        attackArea.center += Vector3.right * -0.7f;
+        attackArea.size = Vector3.one * 0.75f;
+
+        actualBasicAttackCooldown = basicAttackCooldown;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.Log(gameObject.name + " widzi " + other.name);
+        if (Input.GetMouseButtonDown(0) && isPlayer && actualBasicAttackCooldown >= basicAttackCooldown)
+        {
+            other.GetComponent<HP>().TakeHP();
+            Debug.Log("ATACK!!! on " + other.name);
+            StartCoroutine(AttackCooldown());
+        }
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        while (actualBasicAttackCooldown>0)
+        {
+            actualBasicAttackCooldown--;
+            Debug.Log(actualBasicAttackCooldown);
+            yield return new WaitForSeconds(1f);
+        }
+        actualBasicAttackCooldown = basicAttackCooldown;
+    }
+}
