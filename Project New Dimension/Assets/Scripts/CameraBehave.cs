944213417101b9d5fd778028013d79ee58f48ba0
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2022-06-11 20:11:09 +0200

    Dodano wolną kamerę

--- Project New Dimension/Assets/Scripts/CameraBehave.cs
+++ Project New Dimension/Assets/Scripts/CameraBehave.cs
@@ -12,14 +12,20 @@ public class CameraBehave : MonoBehaviour
 
     [SerializeField]
     float duration = 1f;
+
     float elapsed = 0;
     float actualStep;
 
     [SerializeField]
     bool isComingBack = false;
 
+    float xAxis;
+    float yAxis;
+
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+
         gameObject.GetComponent<Collider>().isTrigger = true;
         basicPosition = transform.localPosition;
         currentPosition = basicPosition;
@@ -27,15 +33,21 @@ public class CameraBehave : MonoBehaviour
 
     private void Update()
     {
-        if (!isComingBack) return;
+        if (isComingBack)
+            backToBasicPosition();
 
-        elapsed += Time.deltaTime;
-        actualStep = elapsed / duration;
-        Debug.Log("Oddalanie: " + actualStep);
-        transform.localPosition = Vector3.Lerp(currentPosition, basicPosition, actualStep);
-
-        if(transform.localPosition == basicPosition)
-            isComingBack = false;
+        if (Input.GetMouseButton(1))
+        {
+            xAxis += Input.GetAxis("Mouse X") * 0.1f;
+            yAxis += Input.GetAxis("Mouse Y") * 0.1f;
+            transform.localEulerAngles += new Vector3(-yAxis, 0, 0);
+            transform.parent.localEulerAngles += new Vector3(0, xAxis, 0);
+        }
+        else
+        {
+            xAxis = 0;
+            yAxis = 0;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -59,5 +71,16 @@ public class CameraBehave : MonoBehaviour
         currentPosition = transform.localPosition;
         isComingBack = true;
         elapsed = 0;
+    }
+
+    private void backToBasicPosition()
+    {
+        elapsed += Time.deltaTime;
+        actualStep = elapsed / duration;
+        Debug.Log("Oddalanie: " + actualStep);
+        transform.localPosition = Vector3.Lerp(currentPosition, basicPosition, actualStep);
+
+        if (transform.localPosition == basicPosition)
+            isComingBack = false;
     }
 }
