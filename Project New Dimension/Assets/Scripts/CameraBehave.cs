4c0db5c22bebe0e1170e2caf4b01fcd4bdd5bb4b
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2022-06-11 19:12:00 +0200

    Wprowadzono płynność przybliżania kamery

--- Project New Dimension/Assets/Scripts/CameraBehave.cs
+++ Project New Dimension/Assets/Scripts/CameraBehave.cs
@@ -6,25 +6,58 @@ using UnityEngine;
 public class CameraBehave : MonoBehaviour
 {
     Vector3 basicPosition, currentPosition;
+
+    [SerializeField]
+    Vector3 lowPosition = new Vector3(0.8f, 1.5f);
+
+    [SerializeField]
+    float duration = 1f;
+    float elapsed = 0;
+    float actualStep;
+
+    [SerializeField]
+    bool isComingBack = false;
+
     void Start()
     {
         gameObject.GetComponent<Collider>().isTrigger = true;
         basicPosition = transform.localPosition;
-        //basicPosition = new Vector3(1.4f, 2.1f);
         currentPosition = basicPosition;
-        transform.localPosition = currentPosition;
-        Debug.Log("Start: "+currentPosition);
+    }
+
+    private void Update()
+    {
+        if (!isComingBack) return;
+
+        elapsed += Time.deltaTime;
+        actualStep = elapsed / duration;
+        Debug.Log("Oddalanie: " + actualStep);
+        transform.localPosition = Vector3.Lerp(currentPosition, basicPosition, actualStep);
+
+        if(transform.localPosition == basicPosition)
+            isComingBack = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        elapsed = 0;
+        currentPosition = transform.localPosition;
+        isComingBack = false;
     }
 
     private void OnTriggerStay(Collider other)
     {
-        Debug.Log(currentPosition);
-        transform.localPosition = Vector3.Lerp(currentPosition, new Vector3(0.8f, 1.5f), 100000);
+        elapsed += Time.deltaTime;
+
+        actualStep = elapsed / duration;
+        Debug.Log("Przybli¿anie: "+actualStep);
+        transform.localPosition = Vector3.Lerp(currentPosition, lowPosition, actualStep);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        currentPosition = transform.position;
-        transform.localPosition = Vector3.Lerp(currentPosition, basicPosition, 100000);
+        currentPosition = transform.localPosition;
+        isComingBack = true;
+        elapsed = 0;
     }
 }
